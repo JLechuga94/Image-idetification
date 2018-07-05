8a6c1d0d3b9597dcb86a9fb99356d3a1f7227bb2
Julian Lechuga <julian.lechuga305@gmail.com>
2018-07-05 18:04:45 +0900

    Initialization of project, code for ML data acquisition and analysis

--- AR_AI/AppDelegate.swift
+++ AR_AI/AppDelegate.swift
@@ -1,3 +1,4 @@
+
 //
 //  AppDelegate.swift
 //  AR_AI
--- AR_AI/ViewController.swift
+++ AR_AI/ViewController.swift
@@ -7,14 +7,70 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
+    var imagePicker = UIImagePickerController()
+    var model = GoogLeNetPlaces()
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var descriptionTextView: UITextView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.imagePicker.sourceType = .photoLibrary
+        self.imagePicker.delegate = self
     }
 
-
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let pickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else{
+            return
+        }
+        self.photoImageView.image = pickedImage
+        
+        processImage(image: pickedImage)
+    }
+    
+    func processImage(image: UIImage)  {
+        
+        guard let ciImage = CIImage(image: image) else {
+            fatalError("Unable to create the ciImage object")
+        }
+        guard let visionModel = try? VNCoreMLModel(for: self.model.model) else {
+            fatalError("Unable to create vision model")
+        }
+        
+        let visionRequest = VNCoreMLRequest(model: visionModel){ request, error in
+            if error != nil {
+                return
+            }
+            guard let results = request.results as? [VNClassificationObservation] else {return}
+            
+            let classifications = results.map {observation in
+                "\(observation.identifier)  Confidence: \(observation.confidence*100)%"
+            }
+            
+            
+            DispatchQueue.main.async{
+                print(classifications.count)
+                self.descriptionTextView.text = classifications[0..<3].joined(separator: "\n")
+            }
+        }
+        
+        let visionRequestHandler = VNImageRequestHandler(ciImage: ciImage, options: [:])
+        
+        DispatchQueue.global(qos: .userInteractive).async {
+            try! visionRequestHandler.perform([visionRequest])
+        }
+    }
+    
+    
+    
+    @IBAction func loadImage(_ sender: Any) {
+        self.present(self.imagePicker, animated: true, completion: nil)
+    }
+    
 }
 
