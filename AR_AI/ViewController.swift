eac3964087231a83eadb84ab11a7e83854e1a670
Julian Lechuga <julian.lechuga305@gmail.com>
2018-07-05 16:58:56 +0900

    Initial Commit

---
+++ AR_AI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AR_AI
+//
+//  Created by Julian Lechuga Lopez on 5/7/18.
+//  Copyright © 2018 Julian Lechuga Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
